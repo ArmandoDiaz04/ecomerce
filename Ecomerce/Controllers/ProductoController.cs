58be9b368020a8286bc6bd855565ec44be3d6e77
RaulValencia04 <raulvalenciau@gmail.com>
2023-09-24 16:04:24 -0600

    subasta mood

--- Ecomerce/Controllers/ProductoController.cs
+++ Ecomerce/Controllers/ProductoController.cs
@@ -44,7 +44,6 @@ namespace Ecomerce.Controllers
         }
 
         #endregion
-
         #region GET_ALL - GET
         [HttpGet]
         [Route("GetAll")]
@@ -99,8 +98,98 @@ namespace Ecomerce.Controllers
             return Ok(productos);
         }
         #endregion
+        #region GET_ALL subastas - GET
+        [HttpGet]
+        [Route("GetAllSB")]
+        public ActionResult GetSb([FromQuery] string? q, [FromQuery] int? limit)
+        {
+            // Consulta la base de datos para obtener todos los productos
+            IQueryable<Producto> query = _context.producto.Where(producto => producto.tipo_producto.ToUpper() == "SUBASTA");
 
+            // Filtra por el término 'q' si se proporciona
+            if (!string.IsNullOrEmpty(q))
+            {
+                query = query.Where(p => p.nombre.Contains(q));
+            }
 
+            // Aplica el límite si se proporciona y es un valor positivo
+            if (limit.HasValue && limit.Value > 0)
+            {
+                query = query.Take(limit.Value);
+            }
+
+            // Ejecuta la consulta y obtiene los resultados
+            List<Producto> productos = query.ToList();
+
+            if (productos.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(productos);
+        }
+
+        #endregion
+        #region GET_ALL subastas abierta - GET
+        [HttpGet]
+        [Route("GetSBabiertas")]
+        public ActionResult GetSubastasAbiertas()
+        {
+            // Filtra los productos por estado activo (por ejemplo, estado = 0 es abiertas)
+            IQueryable<Producto> query = _context.producto.Where(p => p.estado == 0 && p.tipo_producto == "subasta");
+
+            // Ejecuta la consulta y obtiene los resultados
+            List<Producto> productos = query.ToList();
+
+            if (productos.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(productos);
+        }
+
+        #endregion
+        #region GET_ALL subasta cerrada - GET
+        [HttpGet]
+        [Route("GetSBcerradas")]
+        public ActionResult GetSubastasCerrada()
+        {
+            // Filtra los productos por estado activo (por ejemplo, estado = 0 es abiertas)
+            IQueryable<Producto> query = _context.producto.Where(p => p.estado == 1 && p.tipo_producto == "subasta");
+
+            // Ejecuta la consulta y obtiene los resultados
+            List<Producto> productos = query.ToList();
+
+            if (productos.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(productos);
+        }
+
+        #endregion
+        #region GET_ALL ventas abiertas - GET
+        [HttpGet]
+        [Route("GetVTabiertas")]
+        public ActionResult GetVentasAbiertas()
+        {
+            // Filtra los productos por estado activo (por ejemplo, estado = 0 es abiertas)
+            IQueryable<Producto> query = _context.producto.Where(p => p.estado == 0 && p.tipo_producto == "venta");
+
+            // Ejecuta la consulta y obtiene los resultados
+            List<Producto> productos = query.ToList();
+
+            if (productos.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(productos);
+        }
+
+        #endregion
         #region GET_BY_ID - GET
         [HttpGet]
         [Route("GetById")]
@@ -113,11 +202,6 @@ namespace Ecomerce.Controllers
             return Ok(producto);
         }
         #endregion
-
-
-
-
-     
         #region AGREGAR - POST
         [HttpPost]
         [Route("add")]
@@ -126,6 +210,12 @@ namespace Ecomerce.Controllers
 
             try
             {
+                if (producto.tipo_producto == "Subasta")
+                {
+                    // Si el tipo de producto es "Subasta", establecer precio_subasta
+                    producto.precio_subasta = producto.Precio;
+                    producto.Precio = 0;
+                }
                 _context.producto.Add(producto);
                 _context.SaveChanges();
 
@@ -139,11 +229,6 @@ namespace Ecomerce.Controllers
 
         }
         #endregion
-
-
-
-
-
         #region ACTUALIZAR-Subasta
         [HttpPut]
         [Route("ActualizarSubasta/{id}")]
@@ -174,10 +259,35 @@ namespace Ecomerce.Controllers
             }
         }
         #endregion
+        #region ACTUALIZAR-Subasta-estado
+        [HttpPut]
+        [Route("ActualizarEstadoSubasta/{id}")]
+        public IActionResult ActualizarEstado(int id)
+        {
+            Producto? producto = _context.producto.Find(id);
 
+            if (producto == null)
+            {
+                return NotFound();
+            }
 
+            // Verifica si la puja es mayor que el precio actual de subasta antes de actualizar
+            if (producto.estado == 0)
+            {
+                // Actualiza el precio de subasta y cualquier otro campo necesario
+                producto.estado = 1;
 
+                _context.Entry(producto).State = EntityState.Modified;
+                _context.SaveChanges();
 
+                return Ok(producto);
+            }
+            else
+            {
+                return BadRequest("La puja debe ser mayor que el precio de subasta actual.");
+            }
+        }
+        #endregion
         #region ACTUALIZAR - POST
 
         [HttpPut]
@@ -212,7 +322,6 @@ namespace Ecomerce.Controllers
         }
 
         #endregion
-
         #region ELIMINAR - DELETE 
         [HttpDelete]
         [Route("deleteUsuario/{id}")]
